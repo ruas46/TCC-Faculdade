eefb5093f57de4824be6b7d874f8dd4f520a40b3
ruas46 <guilhermeruas@hotmail.com>
2017-01-22 19:35:06 -0200

    Estrututando a navegação do site

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def info
+  end
+
+  def graphics
+  end
+
+  def admin
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
+  get '/info' => 'welcome#info'
+  get '/graphics' => 'welcome#graphics'
+  get '/admin' => 'welcome#admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
