c4c26119d6efd1633f78ea1e5e05106557b8179f
ruas46 <guilhermeruas@hotmail.com>
2017-02-01 17:19:33 -0200

    Criado seeds para Sintomas, doenças e uma associação delas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Populando sintomas
+s = Sintoma.new(nome:"Sintoma 1", sintoma_obrigatorio:false)
+s.save
+s = Sintoma.new(nome:"Sintoma 2", sintoma_obrigatorio:false)
+s.save
+s = Sintoma.new(nome:"Sintoma 3", sintoma_obrigatorio:false)
+s.save
+s = Sintoma.new(nome:"Sintoma 4", sintoma_obrigatorio:false)
+s.save
+s = Sintoma.new(nome:"Sintoma 5", sintoma_obrigatorio:true)
+s.save
+
+#Populando Doenças
+d = Doenca.new(nome:"Gripe",min_qtd_sint:"2")
+d.save
+d = Doenca.new(nome:"Dor de Garganta",min_qtd_sint:"1")
+d.save
+
+#Populando a associação sintomas_doenca
+s = Sintoma.new(nome:"Sintoma 6", sintoma_obrigatorio:false)
+s.save
+d = Doenca.new(nome:"Doença 1",min_qtd_sint:1)
+d.save
+d.sintomas_doenca.create(sintoma: s)
+#Para buscas essa associação d.sintomas_doenca.map
