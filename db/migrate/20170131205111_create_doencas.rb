41ee7f16410aa38180528aa520be833ad2c0c620
ruas46 <guilhermeruas@hotmail.com>
2017-05-22 22:45:03 -0300

    adicionado tabela categoria e chave estrangeira na doenca

--- db/migrate/20170131205111_create_doencas.rb
+++ db/migrate/20170131205111_create_doencas.rb
@@ -4,6 +4,7 @@ class CreateDoencas < ActiveRecord::Migration
       t.string :nome
       t.integer :min_qtd_sint
       t.integer :pagina
+      t.belongs_to :categorium, index: { unique: true }, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201193045) do
+ActiveRecord::Schema.define(version: 20170523013443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categoria", force: :cascade do |t|
+    t.string   "nome"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "doencas", force: :cascade do |t|
     t.string   "nome"
