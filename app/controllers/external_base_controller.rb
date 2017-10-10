b0181f327fef0da1f92ca8ea26ea5f3d75db1d38
ruas46 <guilhermeruas@hotmail.com>
2017-10-10 20:30:52 -0300

    adicionada validação em new e edit uma informação de grafico para 10 anos ou 3 estudos

--- app/controllers/external_base_controller.rb
+++ app/controllers/external_base_controller.rb
@@ -19,9 +19,25 @@ class ExternalBaseController < ApplicationController
   def create
     if user_signed_in?
       @acess = @@acess
-      @grafico = Grafico.new(grafico_id: params[:grafico_id], porcentagem: params[:porcentagem], qtd_entrevistados: params[:qtd_entrevistados], ano: params[:ano], estudo: params[:estudo])
-      @grafico.save
-      redirect_to '/'+@@acess+'/graficos'
+      validaAno = []
+      validaEstudo = []
+      validaGrafico = Grafico.where(grafico_id: params[:grafico_id])
+      validaGrafico.each do |x|
+        validaAno << x.ano#pega todos os anos
+        validaEstudo << x.estudo#pega todos os estudos
+      end
+      validaAno << params[:ano]
+      validaEstudo << params[:estudo]
+      validaAno.uniq!#remove os iguais
+      validaEstudo.uniq!#remove os iguais
+
+      if ((validaAno.count > 10)||(validaEstudo.count > 3))#Valida se tem no maximo 10 anos e 3 estudos para n quebrar o grafico
+        redirect_to '/'+@@acess+'/graficos'
+      else
+        @grafico = Grafico.new(grafico_id: params[:grafico_id], porcentagem: params[:porcentagem], qtd_entrevistados: params[:qtd_entrevistados], ano: params[:ano], estudo: params[:estudo])
+        @grafico.save
+        redirect_to '/'+@@acess+'/graficos'
+      end
     else
       redirect_to '/users/sign_in'
     end
@@ -39,14 +55,31 @@ class ExternalBaseController < ApplicationController
   def update
     if user_signed_in?
       @acess = @@acess
-      graficoEdit = Grafico.find(params[:id])#puxa no banco a linha edita e salva
-      graficoEdit.grafico_id = params[:grafico_id]
-      graficoEdit.porcentagem = params[:porcentagem]
-      graficoEdit.qtd_entrevistados = params[:qtd_entrevistados]
-      graficoEdit.ano = params[:ano]
-      graficoEdit.estudo = params[:estudo]
-      graficoEdit.save
-      redirect_to '/'+@@acess+'/graficos'
+
+      validaAno = []
+      validaEstudo = []
+      validaGrafico = Grafico.where(grafico_id: params[:grafico_id])
+      validaGrafico.each do |x|
+        validaAno << x.ano#pega todos os anos
+        validaEstudo << x.estudo#pega todos os estudos
+      end
+      validaAno << params[:ano]
+      validaEstudo << params[:estudo]
+      validaAno.uniq!#remove os iguais
+      validaEstudo.uniq!#remove os iguais
+
+      if ((validaAno.count > 10)||(validaEstudo.count > 3))#Valida se tem no maximo 10 anos e 3 estudos para n quebrar o grafico
+        redirect_to '/'+@@acess+'/graficos'
+      else
+        graficoEdit = Grafico.find(params[:id])#puxa no banco a linha edita e salva
+        graficoEdit.grafico_id = params[:grafico_id]
+        graficoEdit.porcentagem = params[:porcentagem]
+        graficoEdit.qtd_entrevistados = params[:qtd_entrevistados]
+        graficoEdit.ano = params[:ano]
+        graficoEdit.estudo = params[:estudo]
+        graficoEdit.save
+        redirect_to '/'+@@acess+'/graficos'
+      end
     else
       redirect_to '/users/sign_in'
     end
